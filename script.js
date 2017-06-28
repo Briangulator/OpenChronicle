02c8f27048f9529858ead7ee995b7bf4f982db9e
Briangle <brian174174@gmail.com>
2017-06-27 20:41:31 -0400

    Added Power Play

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var open = true;
 function nav() {
 	document.getElementsByTagName("button")[0].innerHTML = open ? "Open Nav" : "Close Nav";
 	document.getElementsByTagName("nav")[0].style.display = open ? "none" : "block";
-	document.getElementsByTagName("div")[0].style.marginRight = open ? "0%" : "12%";
+	document.getElementsByTagName("div")[0].style.marginRight = open ? "0px" : "230px";
 	open = !open;
 }
