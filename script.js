8c2b411f846edfbf139cfbe0a092a937a9911eb7
Briangle <brian174174@gmail.com>
2017-06-29 08:25:06 -0400

    Added Legacy of Evil

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var open = true;
 function nav() {
-	document.getElementsByTagName("button")[0].innerHTML = open ? "Open Nav" : "Close Nav";
+	document.getElementsByTagName("button")[0].innerHTML = open ? "<<" : ">>";
 	document.getElementsByTagName("nav")[0].style.display = open ? "none" : "block";
 	document.getElementsByTagName("div")[0].style.marginRight = open ? "0px" : "230px";
 	open = !open;
