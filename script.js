1560b12674d0fa030d098f21c2a3ce94dcfe5fd4
Briangle <brian174174@gmail.com>
2017-07-11 17:39:37 -0400

    RIP Matoro

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var open = true;
-function nav() {
-	document.getElementsByTagName("button")[0].innerHTML = open ? "<<" : ">>";
-	document.getElementsByTagName("nav")[0].style.display = open ? "none" : "block";
-	document.getElementsByTagName("div")[0].style.marginRight = open ? "0px" : "230px";
-	open = !open;
-}
+/**
+ * OpenChronicle:
+ * script.js
+ * 
+ * Makes the navigation bar open/close button work.
+ */
+window.onload = function() {
+	var isOpen = true,
+		button = document.getElementsByTagName("button")[0],
+		nav = document.getElementsByTagName("nav")[0].style,
+		div = document.getElementsByTagName("div")[0].style;
+	button.onclick = function() {
+		if (isOpen) {
+			button.innerHTML = "<<";
+			nav.display = "none";
+			div.marginRight = "0px";
+		} else {
+			button.innerHTML = ">>";
+			nav.display = "block";
+			div.marginRight = "230px";
+		}
+		isOpen = !isOpen;
+	};
+};
